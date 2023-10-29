56ef73bfc689e4b28e1eccef796a657488bf051f
paularah <paularah.self@gmail.com>
2023-10-29 03:33:25 +0200

    update readme script

--- update-readme.sh
+++ update-readme.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Set the path to the target directory
+target_dir="/home/arah/learning/k8s"
+
 # Remove the existing README.md if it exists
-rm -f README.md
+rm -f "${target_dir}/README.md"
 
 # Create a new README.md file with the initial heading
-echo "# My Kubernetes Learning Notes" > README.md
+echo "# Kubernetes Learning Notes" > "${target_dir}/README.md"
+
+echo "The content here is automically generated from the sub-directories of this repo" > "${target_dir}/README.md"
+
 
 # Function to check if a folder has images
 has_images() {
@@ -20,20 +26,20 @@ has_images() {
 append_images() {
   local folder="$1"
   for image in "${folder}"/*.png "${folder}"/*.jpg "${folder}"/*.jpeg "${folder}"/*.gif "${folder}"/*.bmp "${folder}"/*.svg "${folder}"/*.webp; do
-    echo "![Image](${image})" >> README.md
+    echo "![Image](${image})" >> "${target_dir}/README.md"
   done
 }
 
-# Iterate through subdirectories
-for dir in */; do
+# Iterate through subdirectories of the target directory
+for dir in "${target_dir}"/*/; do
   dir=${dir%*/}  # Remove trailing slash
   if [ -f "${dir}/notes.md" ]; then
-    echo "## $dir" >> README.md
+    echo "## $dir" >> "${target_dir}/README.md"
     if has_images "$dir"; then
       append_images "$dir"
     fi
-    # Append the content from the Notes.md file to README.md
-    cat "${dir}/notes.md" >> README.md
-    echo -e "\n" >> README.md  # Add a newline for separation
+    # Append the content from the notes.md file to README.md
+    cat "${dir}/notes.md" >> "${target_dir}/README.md"
+    echo -e "\n" >> "${target_dir}/README.md"  # Add a newline for separation
   fi
 done
